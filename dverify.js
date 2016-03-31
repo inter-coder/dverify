b2c1786383db01de85b17d056dcdcbf914b3d7f6
Dusan Krstic <admin@inter-coder.com>
2016-03-31 09:54:47 +0200

    Add event pool

--- dverify.js
+++ dverify.js
@@ -3,12 +3,15 @@ var dverify=function(cb){
 	this.about="";
 	this.cb=cb;
 	this.init();
+	this.eventDverifyPool=[];
 };
 dverify.prototype={
 	setValueMethods:function(parent,node){
 		var obj=this;
 		node.addEventListener("keydown", function(e){obj.nodeActive=e.which!=13;});
+		node.addEventListener("keyup", function(e){/*node.dispatchEvent(obj.dverified);*/obj.eventDverifyPool.push(node);});
 		node.addEventListener("click", function(){obj.nodeActive=true;});
+		node.addEventListener("blur", function(){obj.nodeActive=false;});
 		node.readData=function(){
 			if(node.type=="radio"){
 				var chek=parent.querySelectorAll("[type='radio'][field='"+this.getAttribute("field")+"']:checked");
@@ -16,23 +19,23 @@ dverify.prototype={
 			}else if(node.type=="checkbox"){
 				return node.checked?1:0;
 			}
-			return this.value;
+			return this.value==null?"":this.value;
 		};
 		node.setData=function(v){
 			if(node.type=="radio"){
 				var el=parent.querySelector("[type='radio'][field='"+this.getAttribute("field")+"'][value='"+v+"']");
 				if(el!=null){					
-					if(v!=node.readData()){el.dispatchEvent(obj.dverified);}
+					if(v!=node.readData()){/*el.dispatchEvent(obj.dverified);*/obj.eventDverifyPool.push(el);}
 					el.checked=true;
 				}
 			}else if(node.type=="checkbox"){
-				if(v!=node.readData()){node.dispatchEvent(obj.dverified);}
-				node.checked=v==1?true:false;;
+				if(v!=node.readData()){/*node.dispatchEvent(obj.dverified);*/obj.eventDverifyPool.push(node);}
+				node.checked=v==1?true:false;
 			}else{
-				if(v!=node.readData()){node.dispatchEvent(obj.dverified);}
+				if(v!=node.readData()){/*node.dispatchEvent(obj.dverified);*/obj.eventDverifyPool.push(node);}
 				this.value=v;
 			}			
-			return ;
+			return;
 		};
 	},
 	loadstage:function(){
@@ -48,55 +51,58 @@ dverify.prototype={
 			});
 		}
 	},
+	getData:function(){
+		var data={};
+		for(var i in this.elements){
+			data[i]={};
+			for(var x in this.elements[i]){
+				data[i][x]=this.elements[i][x].readData();
+			}
+		}
+		return data;
+	},
+	ciklus:function(){
+		for(var x in this.eventDverifyPool){this.eventDverifyPool[x].dispatchEvent(this.dverified);}
+		this.eventDverifyPool=[];
+		if(JSON.stringify(this.data)!==JSON.stringify(this.getData())){
+			if(this.nodeActive){
+				for(var x in this.elements){
+					for(var i in this.elements[x]){
+						if(this.data[x]!=null){
+							this.data[x][i]=this.elements[x][i].readData();
+						}
+					}
+				}
+			}else{
+				if(this.dataInit){
+					this.data=this.getData();
+					this.dataInit=false;
+				}else{
+					for(var x in this.elements){
+						for(var i in this.elements[x]){
+							if(this.data[x]!=null){
+								this.elements[x][i].setData(this.data[x][i]);
+							}else{
+								this.elements[x][i].setData("");
+							}
+						}
+					}
+				}				
+			}		
+		}
+	},
 	init:function(){
 		var obj=this;
 		this.nodeActive=false;
+		this.dataInit=true;
 		this.data={};
 		this.dveriReady = new Event('dverifyReady');
 		this.dverified = new Event('dverified');
-		Object.defineProperty(this,'_data', {
-			get: function(){
-				var data={};
-				for(var i in this.elements){
-					data[i]={};
-					for(var x in this.elements[i]){
-						data[i][x]=this.elements[i][x].readData();
-					}
-				}
-				if(JSON.stringify(this.data)=="{}"){
-					this.data=JSON.parse(JSON.stringify(data));
-					return data;
-				}else if(JSON.stringify(this.data)===JSON.stringify(data)){					
-					return data;
-				}else{					
-					if(!obj.nodeActive){
-						for(var i in this.data){
-							if(this.data[i]==null){
-								for(var x in this.elements[i]){		
-									this.elements[i][x].setData("");							
-								}
-							}else{
-								for(var x in this.data[i]){		
-									if(this.elements[i][x]!=undefined){
-										if(this.elements[i][x].readData()!=this.data[i][x]){
-											this.elements[i][x].setData(this.data[i][x]);
-										}
-									}							
-								}
-							}							
-						}
-						obj.nodeActive=true;						
-					}else{
-						obj.nodeActive=false;
-						this.data=JSON.parse(JSON.stringify(data));						
-					}
-				}
-			}
-		});
 		this.loadstage();
-		this.timer=setInterval(function(){obj._data;},45);	
-		setTimeout(function(){if(obj.cb!=undefined)obj.cb(obj);document.dispatchEvent(obj.dveriReady);},80);		
+		this.timer=setInterval(function(){obj.ciklus();},145);	
+		setTimeout(function(){if(obj.cb!=undefined)obj.cb(obj);document.dispatchEvent(obj.dveriReady);},80);	
 	}
 };
+
 
 
